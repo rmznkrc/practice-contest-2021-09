c433e97f406e39c2e9225bf646f1aa61850620d0
rmznkrc <68940507+rmznkrc@users.noreply.github.com>
2021-10-19 21:27:09 -0700

    Update Solution1.java

--- Solution1.java
+++ Solution1.java
@@ -6,6 +6,13 @@ public class Solution1 {
   
   
     public static void main(String[] args) {
-        
+        printName("ramazan");
+    }
+      public static void printName(String input){
+        for(int i=0;i<input.length();i++){
+            System.out.println(input.charAt(i));
+
+        }
+     
     }
 }
