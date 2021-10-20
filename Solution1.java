cd829e4e008c1d32087b638313813e7915409438
rmznkrc <68940507+rmznkrc@users.noreply.github.com>
2021-10-19 21:27:48 -0700

    Update Solution1.java

--- Solution1.java
+++ Solution1.java
@@ -8,4 +8,11 @@ public class Solution1 {
     public static void main(String[] args) {
         
     }
+      public static void printName(String input){
+        for(int i=0;i<input.length();i++){
+            System.out.println(input.charAt(i));
+
+        }
+     
+    }
 }
